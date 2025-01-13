8f8cea6faea37775009f8303e494b1dfdaf77f4b
Tristan-Mihai Radulescu <90451752+Courtcircuits@users.noreply.github.com>
2025-01-13 16:17:09 +0100

    feat: code structure (#5)
    * init file structure
    
    * added gitignore

---
+++ producer/target/debug/deps/producer-87c50897eac4927c.d
@@ -0,0 +1,14 @@
+/home/mihai/projects/kafkamion/producer/target/debug/deps/libproducer-87c50897eac4927c.rmeta: src/main.rs src/events/mod.rs src/events/driver.rs src/events/time_registration.rs src/events/truck.rs src/events/position.rs Cargo.toml
+
+/home/mihai/projects/kafkamion/producer/target/debug/deps/producer-87c50897eac4927c.d: src/main.rs src/events/mod.rs src/events/driver.rs src/events/time_registration.rs src/events/truck.rs src/events/position.rs Cargo.toml
+
+src/main.rs:
+src/events/mod.rs:
+src/events/driver.rs:
+src/events/time_registration.rs:
+src/events/truck.rs:
+src/events/position.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ producer/target/debug/deps/producer-df1d29af3811ee75.d
@@ -0,0 +1,10 @@
+/home/mihai/projects/kafkamion/producer/target/debug/deps/libproducer-df1d29af3811ee75.rmeta: src/main.rs src/events/mod.rs src/events/driver.rs src/events/time_registration.rs src/events/truck.rs src/events/position.rs
+
+/home/mihai/projects/kafkamion/producer/target/debug/deps/producer-df1d29af3811ee75.d: src/main.rs src/events/mod.rs src/events/driver.rs src/events/time_registration.rs src/events/truck.rs src/events/position.rs
+
+src/main.rs:
+src/events/mod.rs:
+src/events/driver.rs:
+src/events/time_registration.rs:
+src/events/truck.rs:
+src/events/position.rs:
